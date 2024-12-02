6fed78fe694b949edde4048d0397e1cf16052889
Stephen Molloy <sdmolloy@gmail.com>
2024-12-03 00:08:46 +0100

    Day 03 prep done

---
+++ day_03/src/main.c
@@ -0,0 +1,34 @@
+#include <assert.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SDM_LIB_IMPLEMENTATION
+#include "sdm_lib.h"
+
+typedef struct {
+  size_t capacity;
+  size_t length;
+  int *data;
+} DynIntArray;
+
+typedef struct {
+  size_t capacity;
+  size_t length;
+  DynIntArray *data;
+} DynDynIntArray;
+
+int main(void) {
+  // char *input_file = "./test.txt";
+  // char *input_file = "./input.txt";
+  char *input_file = "./src/main.c";
+  char *file_contents = sdm_read_entire_file(input_file);
+  sdm_string_view contents_view = sdm_cstr_as_sv(file_contents);
+
+  printf(SDM_SV_F, SDM_SV_Vals(contents_view));
+
+  free(file_contents);
+
+  return 0;
+}
+
