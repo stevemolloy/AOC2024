8c575a4718005cce012aa14a6ac99b88b14247cb
Stephen Molloy <sdmolloy@gmail.com>
2024-12-03 11:45:44 +0100

    Day 3 done

--- day_03/src/main.c
+++ day_03/src/main.c
@@ -1,28 +1,61 @@
+#include <ctype.h>
 #include <stdio.h>
 
 #define SDM_LIB_IMPLEMENTATION
 #include "sdm_lib.h"
 
-typedef struct {
-  size_t capacity;
-  size_t length;
-  int *data;
-} DynIntArray;
-
-typedef struct {
-  size_t capacity;
-  size_t length;
-  DynIntArray *data;
-} DynDynIntArray;
-
 int main(void) {
   // char *input_file = "./test.txt";
-  // char *input_file = "./input.txt";
-  char *input_file = "./src/main.c";
+  // char *input_file = "./test2.txt";
+  char *input_file = "./input.txt";
   char *file_contents = sdm_read_entire_file(input_file);
   sdm_string_view contents_view = sdm_cstr_as_sv(file_contents);
 
-  printf(SDM_SV_F, SDM_SV_Vals(contents_view));
+  for (size_t i=0; i<contents_view.length; i++) {
+    if (contents_view.data[i] == '\n') {
+      contents_view.data[i] = ' ';
+    }
+  }
+
+  size_t i = 0;
+  int val1, val2;
+  int part1_ans = 0, part2_ans = 0;
+  int enabled = 1;
+  while (i<contents_view.length) {
+    if (strncmp(contents_view.data + i, "do()", 4) == 0)         enabled = 1;
+    else if (strncmp(contents_view.data + i, "don't()", 7) == 0) enabled = 0;
+    val1 = 0;
+    val2 = 0;
+    if (strncmp(contents_view.data + i, "mul(", 4) == 0) {
+      // Found mul(
+      i += 4;
+      size_t numdigits = 0;
+      while (isdigit(contents_view.data[i])) {
+        numdigits++;
+        val1 *= 10;
+        val1 += contents_view.data[i] - '0';
+        i++;
+      }
+      if (numdigits > 3) continue;
+      if (contents_view.data[i] != ',') continue;
+      i++;
+      numdigits = 0;
+      while (isdigit(contents_view.data[i])) {
+        numdigits++;
+        val2 *= 10;
+        val2 += contents_view.data[i] - '0';
+        i++;
+      }
+      if (numdigits > 3) continue;
+      if (contents_view.data[i] != ')') continue;
+      part1_ans += val1 * val2;
+      part2_ans += enabled * val1 * val2;
+    }
+    i++;
+  }
+
+  printf("Answer to part 1 = %d\n", part1_ans);
+  printf("Answer to part 2 = %d\n", part2_ans);
 
   free(file_contents);
 
