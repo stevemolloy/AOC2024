a62c1581c7bddf996faadd4c1a22a45ef8c9b171
Stephen Molloy <sdmolloy@gmail.com>
2024-12-08 18:52:20 +0000

    Day 8 both parts

--- day_08/src/main.c
+++ day_08/src/main.c
@@ -3,6 +3,8 @@
 #define SDM_LIB_IMPLEMENTATION
 #include "sdm_lib.h"
 
+int b_gcd(int num1, int num2);
+
 typedef struct {
   size_t capacity;
   size_t length;
@@ -12,6 +14,13 @@ typedef struct {
 typedef struct {
   int rownum, colnum;
 } Coord;
+
+bool coord_in_grid(Coord x, int numrows, int numcols) {
+  return (
+    (0 <= x.rownum) && (x.rownum < numrows) &&
+    (0 <= x.colnum) && (x.colnum < numcols)
+  );
+}
 
 Coord coord_add(Coord a, Coord b) {
   return (Coord){
@@ -25,6 +34,19 @@ Coord coord_sub(Coord a, Coord b) {
     .rownum = a.rownum - b.rownum,
     .colnum = a.colnum - b.colnum,
   };
+}
+
+Coord coord_scale(Coord x, int c) {
+  x.rownum *= c;
+  x.colnum *= c;
+  return x;
+}
+
+Coord coord_simplify(Coord a) {
+  int gcd = b_gcd(a.rownum, a.colnum);
+  a.rownum /= gcd;
+  a.colnum /= gcd;
+  return a;
 }
 
 typedef struct {
@@ -104,7 +126,6 @@ int main(void) {
     return 1;
   }
   strcpy(grid_copy, file_contents);
-
   sdm_string_view grid_copy_sv = sdm_cstr_as_sv(grid_copy);
 
   SVArray gridrows = {0};
@@ -158,6 +179,45 @@ int main(void) {
   }
   printf("Part 1 = %zu\n", part1_ans);
 
+  printf("%s\n", file_contents);
+  strcpy(grid_copy, file_contents);
+
+  for (size_t i=0; i<NUMCOORDS; i++) {
+    if (coords[i].length ==0) continue;
+    CoordArray c_arr = coords[i];
+    for (size_t j=0; j<c_arr.length; j++) {
+      Coord start = c_arr.data[j];
+      for (size_t k=0; k<c_arr.length; k++) {
+        if (j == k) continue;
+        Coord end = c_arr.data[k];
+        Coord diff = coord_simplify(coord_sub(end, start));
+        int scale = 0;
+        Coord new_coord = coord_add(start, coord_scale(diff, scale));
+        while (coord_in_grid(new_coord, gridrows.length, gridrows.data[0].length)) {
+          gridrows.data[new_coord.rownum].data[new_coord.colnum] = '#';
+          scale++;
+          new_coord = coord_add(start, coord_scale(diff, scale));
+        }
+        scale = -1;
+        new_coord = coord_add(start, coord_scale(diff, scale));
+        while (coord_in_grid(new_coord, gridrows.length, gridrows.data[0].length)) {
+          gridrows.data[new_coord.rownum].data[new_coord.colnum] = '#';
+          scale--;
+          new_coord = coord_add(start, coord_scale(diff, scale));
+        }
+      }
+    }
+  }
+
+  printf("%s\n", grid_copy);
+
+  size_t part2_ans = 0;
+  for (size_t i=0; i<strlen(grid_copy); i++) {
+    if (grid_copy[i] == '#') {
+      part2_ans++;
+    }
+  }
+  printf("Part 2 = %zu\n", part2_ans);
   free(file_contents);
 
   return 0;
